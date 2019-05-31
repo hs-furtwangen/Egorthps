ac46a7bd02f57c848648b091aab229e993ab0f4e
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-31 20:13:20 +0200

    Added first orthographic demo scene with basic player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 10.0f;
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 positionVector = transform.position;
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        Vector3 position = rigidbody.position;
+        position = position + new Vector3(xAxis, 0, zAxis) * playerSpeed * Time.deltaTime;
+        rigidbody.MovePosition(position);
+    }
+}
