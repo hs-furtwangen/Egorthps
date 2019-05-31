261e12028c6a511f71098296808d365f2b63a42e
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-31 20:37:54 +0200

    Fixed script reference name

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     //Public
     public float playerSpeed = 10.0f;
