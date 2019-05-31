46345e7fa47dc3ac69850d0d4fd66186a802a96e
Alexander Scheurer <mail@aspepex.net>
2019-05-31 22:52:38 +0200

    Posprocessing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,39 +7,39 @@ public class PlayerController : MonoBehaviour
 
     public float cameraSpeedH = 2.0f;
     public float cameraSpeedV = 2.0f;
-    public Camera playerCamera;
 
-    //Private
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    private Vector3 mov;
+    private Vector3 rot;
 
-    Rigidbody rigidbody;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+
+        mov = Vector3.zero;
+        rot = Vector3.zero;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Camera Movement
-        yaw += cameraSpeedH * Input.GetAxis("Mouse X");
-        pitch -= cameraSpeedV * Input.GetAxis("Mouse Y");
-        playerCamera.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        rot.y += cameraSpeedH * Input.GetAxis("Mouse X");
+        rot.x -= cameraSpeedV * Input.GetAxis("Mouse Y");
+        this.transform.rotation = Quaternion.Euler(rot);
 
-        //Player body movement
-        Vector3 position = rigidbody.position;
+        mov = this.transform.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime + this.transform.forward * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        
+        rb.MovePosition(rb.position + mov);
 
-        float xAxis = Input.GetAxis("Horizontal");
-        float zAxis = Input.GetAxis("Vertical");
-
-        Vector3 targetDirection = new Vector3(xAxis, 0f, zAxis);
-        targetDirection = playerCamera.transform.TransformDirection(targetDirection);
-        targetDirection.y = 0.0f;
-
-        position = position + targetDirection * playerSpeed * Time.deltaTime;
-        rigidbody.MovePosition(position);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(this.transform.up * 3, ForceMode.Impulse);
+        }
     }
 }
