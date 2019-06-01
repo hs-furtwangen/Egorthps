ed9176963d8083cf4f5f197b6af10aa586da09d4
Bastian Hodapp <bastianhodapp@gmail.com>
2019-06-01 22:18:52 +0200

    add sound source to level goal

---
+++ Assets/LevelGoal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour
+{
+    public AudioSource levelFinishedAudioSource;
+
+    void OnCollisionEnter(Collision collision) {
+        
+    }
+}
