26b3702bb8d062db10f32fca4f713886a5aeefdc
Bastian Hodapp <bastianhodapp@gmail.com>
2019-06-01 15:56:25 +0200

    Added UI with Time display

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject uiCanvas;
+
+    private GameObject uiCanvasInstance;
+    private float startTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = Time.time;
+        uiCanvasInstance = Instantiate(uiCanvas);
     }
 
     // Update is called once per frame
     void Update()
     {
+        uiCanvasInstance.GetComponentInChildren<Text>().text = "Time: " + Math.Round(Time.timeSinceLevelLoad, 2);
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
