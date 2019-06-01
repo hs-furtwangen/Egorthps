270cc4f6b997062a9d0b1685b4f9925c3301c468
Bastian Hodapp <bastianhodapp@gmail.com>
2019-06-01 11:45:02 +0200

    Updated perspective switcher and player camera size

--- Assets/Scripts/PerspectiveSwitcher.cs
+++ Assets/Scripts/PerspectiveSwitcher.cs
@@ -1,37 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
- 
-[RequireComponent (typeof(MatrixBlender))]
+
+[RequireComponent(typeof(MatrixBlender))]
 public class PerspectiveSwitcher : MonoBehaviour
 {
-    private Matrix4x4   ortho,
-                        perspective;
-    public float        fov, near, far, orthographicSize;
-    private float       aspect;
+    private Matrix4x4 ortho, perspective;
+    public float fov, near, far, orthographicSize;
+    private float aspect;
     private MatrixBlender blender;
-    private bool        orthoOn;
+    private bool orthoOn;
 
     private Camera camera;
- 
+
     void Start()
     {
         camera = GetComponent<Camera>();
+        blender = GetComponent<MatrixBlender>();
 
+        //Get current camera ortho data
         fov = camera.fieldOfView;
         near = camera.nearClipPlane;
         far = camera.farClipPlane;
         orthographicSize = camera.orthographicSize;
 
-        aspect = (float) Screen.width / (float) Screen.height;
+        aspect = (float)Screen.width / (float)Screen.height;
 
         ortho = Matrix4x4.Ortho(-orthographicSize * aspect, orthographicSize * aspect, -orthographicSize, orthographicSize, near, far);
         perspective = Matrix4x4.Perspective(fov, aspect, near, far);
 
         camera.projectionMatrix = ortho;
         orthoOn = true;
-        blender = (MatrixBlender) GetComponent(typeof(MatrixBlender));
     }
- 
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
