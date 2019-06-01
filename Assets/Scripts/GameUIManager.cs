250338fc00e7fa0c9198e8a808200800ae2d8303
Bastian Hodapp <bastianhodapp@gmail.com>
2019-06-01 18:52:39 +0200

    Streamlined UIManager

---
+++ Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    public static GameUIManager Instance;
+    public Text timeText;
+
+    void Start() {
+        Instance = this;
+    }
+
+    public void SetTimeText(string newText) {
+        timeText.text = newText;
+    }
+}
