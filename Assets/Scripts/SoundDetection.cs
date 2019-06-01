4a7fff4a7e7dc38aa90d1d07533970a4046599c1
Lukas Scheuerle <lukas.scheuerle@web.de>
2019-06-01 18:16:47 +0200

    Beat detection 🎶

---
+++ Assets/Scripts/SoundDetection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDetection : MonoBehaviour
+{
+    AudioSource audioSource;
+    LineRenderer lr;
+    public BeatDetection beatDetection; //add GO with the BeatDetection Script in here. Add BD Script to Main Camera
+    float cooldown = 0.4f;
+    float currentCooldown = 0f;
+
+    Material mat;
+
+    void Start()
+    {
+
+
+        beatDetection.CallBackFunction = OnBeatDetected;
+
+        mat = this.GetComponent<Renderer>().material;
+    }
+
+    void OnBeatDetected(BeatDetection.EventInfo eventInfo)
+    {
+        //eventInfo.messageInfo == BeatDetection.EventType.Kick
+        if (currentCooldown < 0 && eventInfo.messageInfo == BeatDetection.EventType.Snare)
+        {
+            mat.color = Color.HSVToRGB(Random.value, 1, 1);
+            currentCooldown = cooldown;
+        }
+    }
+
+    private void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+
+    }
+}
