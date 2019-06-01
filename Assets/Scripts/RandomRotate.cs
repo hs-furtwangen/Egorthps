8d38fb3cf5deb54036376ec93d1cdb00e00ad9e5
Bastian Hodapp <bastianhodapp@gmail.com>
2019-06-01 21:19:50 +0200

    add cube with random rotation

---
+++ Assets/Scripts/RandomRotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public class RandomRotate : MonoBehaviour
+{
+    public float xRotationSpeedMax, xRotationSpeedMin;
+    public float yRotationSpeedMax, yRotationSpeedMin;
+    public float zRotationSpeedMax, zRotationSpeedMin;
+
+
+    private float xRotationSpeed;
+    private float yRotationSpeed;
+    private float zRotationSpeed;
+
+    void Start() {
+        UnityEngine.Random.InitState(gameObject.GetInstanceID());
+        xRotationSpeed = UnityEngine.Random.Range(xRotationSpeedMin, xRotationSpeedMax);
+        yRotationSpeed = UnityEngine.Random.Range(yRotationSpeedMin, yRotationSpeedMax);
+        zRotationSpeed = UnityEngine.Random.Range(zRotationSpeedMin, zRotationSpeedMax);
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(xRotationSpeed, yRotationSpeed, zRotationSpeed) * Time.deltaTime);
+    }
+}
