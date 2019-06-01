56e47109fdfd00e578765e31d3906d2d5bd4a576
Bastian Hodapp <bastianhodapp@gmail.com>
2019-06-01 14:12:37 +0200

    Add grounded check to player jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,19 @@ public class PlayerController : MonoBehaviour
     public float cameraSpeedH = 2.0f;
     public float cameraSpeedV = 2.0f;
 
+    public float jumpForce = 5.0f;
+
     private Vector3 mov;
     private Vector3 rot;
 
+    float distToGround;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+
         rb = GetComponent<Rigidbody>();
 
         mov = Vector3.zero;
@@ -34,17 +39,21 @@ public class PlayerController : MonoBehaviour
         this.transform.rotation = Quaternion.Euler(rot);
 
         mov = this.transform.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime + this.transform.forward * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
-        
+
         rb.MovePosition(rb.position + mov);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            rb.AddForce(this.transform.up * 3, ForceMode.Impulse);
+            rb.AddForce(this.transform.up * jumpForce, ForceMode.Impulse);
         }
 
         if (Input.GetKeyDown(KeyCode.C))
         {
             Camera.main.orthographic = !Camera.main.orthographic;
         }
+    }
+
+    bool IsGrounded() {
+    return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
 }
