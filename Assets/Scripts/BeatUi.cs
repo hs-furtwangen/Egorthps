1c5c7c8deef594b034d1ff4dff669622d0bc9a3f
Alexander Scheurer <mail@aspepex.net>
2019-06-02 16:08:42 +0200

    Title + beat interface

--- Assets/Scripts/BeatUi.cs
+++ Assets/Scripts/BeatUi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BeatUi : MonoBehaviour
+public class BeatUi : MonoBehaviour, IBeatObject
 {
     public Color[] Colors =
     {
