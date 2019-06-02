5f49d0e6d837db9db68bbf8ff2d2b4d98404c455
Alexander Scheurer <mail@aspepex.net>
2019-06-02 14:01:09 +0200

    Tinyfixes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using System;
 
 public class GameController : MonoBehaviour
