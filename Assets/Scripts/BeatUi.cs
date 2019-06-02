54d1c3f0af95d69998c162fcb320e8b768d533b4
Alexander Scheurer <mail@aspepex.net>
2019-06-02 13:34:35 +0200

    Credits

---
+++ Assets/Scripts/BeatUi.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatUi : MonoBehaviour
+{
+    public Color[] Colors =
+    {
+        Color.white,
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.yellow,
+        Color.magenta
+    };
+
+    public float duration = 0.1f;
+
+    public Color lastColor;
+    public Color currentColor;
+    public Color nextColor;
+
+    public Image img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.color = currentColor;
+    }
+
+    IEnumerator SwitchColor()
+    {
+        float startTime = Time.time;
+        while (Time.time - startTime < duration)
+        {
+            currentColor = Color.Lerp(lastColor, nextColor, (Time.time - startTime) / duration);
+            yield return null;
+        }
+
+        lastColor = currentColor;
+    }
+
+    public void NextColor()
+    {
+        StopAllCoroutines();
+
+        nextColor = Colors[Mathf.RoundToInt(Random.Range(0, Colors.Length - 1))];
+
+        while (lastColor == nextColor)
+        {
+            nextColor = Colors[Mathf.RoundToInt(Random.Range(0, Colors.Length - 1))];
+        }
+
+        lastColor = img.color;
+
+        StartCoroutine(SwitchColor());
+    }
+}
